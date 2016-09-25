800e308fc6220256942d55de7371feec7b0d5597
mies <mies@wercker.com>
2016-09-25 11:08:09 +0200

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({cities : ["San Francisco","Amsterdam", "Berlin", "New York", "Palo ALto", "San Mateo"]}));
+    res.end();
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+
+module.exports = app;
